6eada21f3f317bcd30f19a62413bcc3b11de7133
RosyGraph <chris.stevenson@live.com>
2023-07-14 13:08:13 -0600

    Test filtering and aggregating

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import os
+
+import pytest
+
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+
+
+@pytest.fixture()
+def llm():
+    return OpenAI(api_token=os.getenv("OPENAI_API_TOKEN"))
+
+
+@pytest.fixture()
+def pandas_ai(llm):
+    return PandasAI(llm, verbose=True, conversational=False)
---
+++ tests/test_only_df.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import pytest
+
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+
+
+@pytest.mark.skip(reason="Don't hit the API")
+def test_filter_date(pandas_ai):
+    df = pd.DataFrame(
+        {
+            "date": ["2020-01-01", "2020-01-02", "2021-01-03", "2021-01-04", "2021-01-05", "2021-01-06"],
+            "cost": [1, 2, 3, 4, 5, 6],
+        }
+    )
+    filtered_df = pandas_ai(df, prompt="Show me the cost for 2020")
+    assert type(filtered_df) == pd.DataFrame
+    assert filtered_df.shape == (2, 2)
+
+
+@pytest.mark.skip(reason="Don't hit the API")
+def test_aggregate_rounds(pandas_ai):
+    df = pd.DataFrame(
+        {
+            "date": ["2020-01-01", "2020-01-02", "2021-01-03", "2021-01-04", "2021-01-05", "2021-01-06"],
+            "round": [1, 1, 2, 2, 3, 3],
+        }
+    )
+    filtered_df = pandas_ai(df, prompt="How many investments are made in different rounds?")
+    assert type(filtered_df) == pd.DataFrame
+    assert filtered_df.shape == (3, 2)
+    assert filtered_df.columns.tolist() == ["round", "count"]
+    assert filtered_df["count"].tolist() == [2, 2, 2]
+
+
+@pytest.mark.skip(reason="Don't hit the API")
+def test_filter_and_aggregate(pandas_ai):
+    df = pd.DataFrame(
+        {
+            "date": ["2020-01-01", "2020-01-02", "2021-01-03", "2021-01-04", "2021-01-05", "2021-01-06"],
+            "round": [1, 1, 2, 2, 3, 3],
+        }
+    )
+    filtered_df = pandas_ai(df, prompt="How many investments are made in different rounds in 2021?")
+    assert type(filtered_df) == pd.DataFrame
+    assert filtered_df.shape == (2, 2)
+    assert filtered_df.columns.tolist() == ["round", "count"]
+    assert filtered_df["count"].tolist() == [2, 2]
+
+
+@pytest.mark.skip(reason="Don't hit the API")
+def test_filter_lots_of_columns(pandas_ai):
+    df = pd.DataFrame(
+        {
+            "date": ["2020-01-01", "2020-01-02", "2021-01-03", "2021-01-04", "2021-01-05", "2021-01-06"],
+            "round": [1, 1, 2, 2, 3, 3],
+            "company": ["A", "A", "B", "B", "C", "C"],
+            "investor": ["G", "H", "I", "J", "K", "L"],
+            "bananas": [3, 2, 3, 4, 5, 6],
+            "apples": [2, 2, 3, 4, 5, 6],
+            "oranges": [1, 2, 3, 4, 5, 6],
+            "pears": [1, 2, 3, 4, 5, 6],
+            "grapes": [1, 2, 3, 4, 5, 6],
+            "pineapples": [1, 2, 3, 4, 5, 2],
+            "mangoes": [1, 2, 3, 4, 5, 6],
+        }
+    )
+    filtered_df = pandas_ai(df, prompt="Show me bananas by company sorted low to high")
+    print(filtered_df)
+    assert type(filtered_df) == pd.DataFrame
+    assert filtered_df.shape == (4, 2)
+    assert False
+
+
+@pytest.mark.skip(reason="Don't hit the API")
+def test_average_banana(pandas_ai):
+    df = pd.DataFrame(
+        {
+            "date": ["2020-01-01", "2020-01-02", "2021-01-03", "2021-01-04", "2021-01-05", "2021-01-06"],
+            "round": [1, 1, 2, 2, 3, 3],
+            "company": ["A", "A", "B", "B", "C", "C"],
+            "investor": ["G", "H", "I", "J", "K", "L"],
+            "bananas": [3, 2, 3, 4, 5, 6],
+            "apples": [2, 2, 3, 4, 5, 6],
+            "oranges": [1, 2, 3, 4, 5, 6],
+            "pears": [1, 2, 3, 4, 5, 6],
+            "grapes": [1, 2, 3, 4, 5, 6],
+            "pineapples": [1, 2, 3, 4, 5, 2],
+            "mangoes": [1, 2, 3, 4, 5, 6],
+        }
+    )
+    filtered_df = pandas_ai(df, prompt="What is the variance of bananas as a percentage per company?")
+    print(filtered_df)
+    assert type(filtered_df) == pd.DataFrame
+    assert filtered_df.shape == (4, 2)
+    assert False
